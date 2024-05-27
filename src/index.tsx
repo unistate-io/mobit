2a4cf7aef78eff550508f0d8574ba60d8d3427d1
qieman <253058016@qq.com>
2024-05-27 17:46:33 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/app.scss';
+import { RouterProvider } from "react-router-dom";
+import router from "./pages/router";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
