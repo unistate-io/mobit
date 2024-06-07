dc11a0c07f62cb9a9bc40d45e7d6d6fb77b4c40a
qieman <253058016@qq.com>
2024-06-07 11:44:31 +0800

    update search

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, {forwardRef} from "react";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
     startIcon?: React.ReactNode
     endIcon?: React.ReactNode
 }
 
-export default function Input({startIcon, endIcon, ...props}: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(({startIcon, endIcon, className, ...props}, ref) => {
     return <div className="rounded-xl bg-[#F8F9F8] flex flex-row  items-center px-4">
         {
             !!startIcon && <div className="mr-2 flex flex-row items-center">{startIcon}</div>
         }
-        <input className="rounded-xl outline-0 flex-1 bg-inherit py-4 text-4" {...props }/>
+        <input className={`rounded-xl !outline-0 flex-1 bg-inherit py-4 text-4 ${className}`} {...props } ref={ref}/>
         {
             !!endIcon && <div className="ml-2">{endIcon}</div>
         }
     </div>
-}
+})
+
+export default Input
--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,25 +1,73 @@
 import Avatar from "@/components/Avatar/Avatar";
 import {getTheme} from "@/providers/UserProvider/themes";
-import {useContext, useEffect} from "react";
-import {CKBContext} from "@/providers/CKBProvider/CKBProvider";
-import {Link} from "react-router-dom";
-import Select from "@/components/Select/Select";
-import {LangContext} from "@/providers/LangProvider/LangProvider";
-import {useNavigate} from "react-router-dom";
+import {useContext, useEffect, useState, useRef} from "react";
+import {CKBContext} from "@/providers/CKBProvider/CKBProvider"
+import {Link} from "react-router-dom"
+import Select from "@/components/Select/Select"
+import {LangContext} from "@/providers/LangProvider/LangProvider"
+import {useNavigate} from "react-router-dom"
+import Input from "@/components/Form/Input/Input"
+import {checksumCkbAddress} from "@/utils/common"
+import {ToastContext, ToastType} from "@/providers/ToastProvider/ToastProvider"
 
 export default function HeaderMenu() {
     const {open, address} = useContext(CKBContext);
     const {lang, langType, switchLang} = useContext(LangContext)
     const navigate = useNavigate()
+    const {showToast} = useContext(ToastContext)
+
+    const searchRef = useRef<HTMLInputElement | null>(null)
+
+    const [showSearchInput, setShowSearchInput] = useState(false)
+
+    const showSearch = () => {
+        setShowSearchInput(true)
+        setTimeout(() => {
+            searchRef.current?.focus()
+        }, 100)
+    }
 
     useEffect(() => {
         if (!!address) {
-            navigate(`address/${address}`)
+            navigate(`/address/${address}`)
         }
     }, [address])
 
     return <div className="flex flex-row flex-nowrap items-center">
-        <Link to={'search'} className="text-sm"><i className="uil-search"/></Link>
+        <div className="text-sm relative flex flex-row items-center">
+            {
+                showSearchInput &&
+                <div className="absolute right-0">
+                    <Input
+                        onKeyUp={(e) => {
+                            if (e.key === 'Enter') {
+                                if (checksumCkbAddress(e.currentTarget.value)) {
+                                    navigate(`/address/${e.currentTarget.value}`)
+                                    e.currentTarget.blur()
+                                    setShowSearchInput(false)
+                                } else {
+                                    showToast('Invalid address', ToastType.error)
+                                }
+                            }
+
+                            if (e.key=== 'Escape') {
+                                setShowSearchInput(false)
+                            }
+                        }}
+                        ref={searchRef}
+                        startIcon={<i className="uil-search"/>}
+                        endIcon={<img src={'/images/icon_esc.svg'} alt={""} />}
+                        className={'h-30 w-[250px] !py-2'}
+                        placeholder="Search..."
+                        onBlur={() => setShowSearchInput(false)}
+                    />
+                </div>
+            }
+            {
+                !showSearchInput &&
+                <i className="uil-search" onClick={showSearch}/>
+            }
+        </div>
         <i className="w-[1px] h-[14px] bg-black mx-3 scale-x-50 hidden sm:block "/>
         <div className="hidden sm:block text-xs">
             <Select
