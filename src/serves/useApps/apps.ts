5d32b60343d528002d392ee1a697240589a5c6fd
qieman <253058016@qq.com>
2024-06-21 08:51:52 +0800

    Update apps.ts

--- src/serves/useApps/apps.ts
+++ src/serves/useApps/apps.ts
@@ -37,31 +37,31 @@ const apps: CkbApp[] = [
         description: 'CKB Assets Manager'
     },
     {
-        logo: 'https://v5.airtableusercontent.com/v3/u/30/30/1718820000000/g2ibkUbHhcbt-LGgybArYQ/DESr2_mffzr1HiS05uoIHyv9w9_rZHmh9vyPkwdz8_hehXSYYrZsTPr6UZl4LMX7FfZBvk_wNmoD3teLIqnU0pkpprJ2tT9r86PlqmXeMr4xug_mQSoGVaYC70et610rtMW3Y2xgM-hsCZDslHAwJQ/jjN_A-_olruNCyiQ6MLR8E6voKyIcLyQSW_k0eBf6Bg',
+        logo: 'https://ik.imagekit.io/soladata/ol84miug_8QnlkFMT7',
         name: 'Nervape',
         url: 'https://www.nervape.com/nervape',
         description: 'Nervape, multi-chain composable digital objects built on bitcoin.'
     },
     {
-        logo: 'https://v5.airtableusercontent.com/v3/u/30/30/1718820000000/UoAikfkBYq5nmRGX-u92RA/naCcj6_ap0jaVhjFqTuavZFF9vLWZ3qcUBAKnpI6-NUB2fPJ-HWszkiDMsp_CtZmh1VNqwa9NT5sZ7mWRPjuW5oXoIAl79a15MdcbTbf4zGHs1zxgbq_YTi8s7K8gbI8p1MfG7cfTCV_jw2FuapSWQ/q0Y2L4y0wgtBRGUPneYbnS6UJLJ7T5KlAE1GPnIznYU',
+        logo: 'https://ik.imagekit.io/soladata/c6hmnacl_hgSArXWdmX',
         name: 'Haste',
         url: 'https://haste.pro',
         description: 'Haste是一个资产管理器，可以方便地管理您在 Bitcoin/RGB++/CKB 上的加密资产。'
     },
     {
-        logo: 'https://v5.airtableusercontent.com/v3/u/30/30/1718820000000/aISM73SzC_PK7_6K6SUYoA/0BiKXzhKqnMDWz45vBIdeu6y0y3Ua4lDjQvIUVKceIZOpg1x5RRq-_Z3wAoMMw_5DTGidfvQBPnkXaQPwmHOPYfirK8cIQZzj-_9LYzQVpQy8sl5eMOzBM7vfEJnP1LISmEMI7b5RQR4qKtwd1XAVQ/OGq_KSRhQKPgknrjX4-0fzB6J3wFvSWbkCaAUvb6Fq4',
+        logo: 'https://ik.imagekit.io/soladata/h8m5xerk_obW7ga3Lve',
         name: 'Rei Wallet',
         url: 'https://reiwallet.io',
         description: 'CKB 原生的浏览器插件钱包'
     },
     {
-        logo: 'https://v5.airtableusercontent.com/v3/u/30/30/1718820000000/zbgUD6QQPSZRDPX1X_PGzw/PX1EV1tDiu9WWxATg5ylc9R95cnu-7nAR-RzuiovJJtQVXwMzHzvPJgkaJEPISvR9aJmzirGVwTMDbVyaNKmohdQy1fLuPCuqdywwcmoqadE-5ZVm-uQlyCJSvsFvoYyBJxDcKkWD8n4RqqEhP_AUw/TNy9ei5zS5Fo8GttZmjdmNJ1ptjeccGDC_IjwJ44ucg',
+        logo: 'https://ik.imagekit.io/soladata/zwzysaqq_-ReLqUcGT',
         name: 'Cell Script',
         url: 'https://cellscript.io',
         description: 'CKB上的智能合约语言。通过使用Cell Script，开发人员可以在几分钟内开始编写CKB智能合约。'
     },
     {
-        logo: 'https://v5.airtableusercontent.com/v3/u/30/30/1718820000000/VmFCAnrfRnNhKNSMUweNPQ/V5bTUYwSbyA7zwp3Oe0Rq1_TCQHDGlukMr0ihku7Vupj_j-v78tBK7_rQj5y8hCppulnN1OAq7QkhtaWj_tFo2oHva7lp3wKOG0fQgml7VNhqvy862FYVR-XHgtJyvmmXCceRQelBo49FdPbVheCWw/elahVWd_m7vAFvDc4rUKk31cCjDbOhMFo_bZMM4SXlk',
+        logo: 'https://ik.imagekit.io/soladata/7floqz53_Ui8LEVZF1',
         name: 'Dobby',
         url: 'https://app.dobby.market',
         description: 'Dobby 是 Bitcion 上的数码物（DOBs）去中心化交易平台'
