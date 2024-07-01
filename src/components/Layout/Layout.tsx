1b5387532f25b1986694beba0be89c775c346b01
qieman <253058016@qq.com>
2024-07-01 20:51:22 +0800

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React, {useLayoutEffect} from 'react'
+import {useLocation} from "react-router-dom"
 import Header from "@/components/Header/Header"
 import CKBProvider from "@/providers/CKBProvider/CKBProvider"
 // @ts-ignore
-import { KeepAliveOutlet } from 'react-alive-outlet'
+import {KeepAliveOutlet} from 'react-alive-outlet'
+
 
 function Home() {
+    const location = useLocation()
+
+    useLayoutEffect(() => {
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+        scrollTop && window.scrollTo(0, 0)
+    }, [location.pathname])
+
     return (
         <CKBProvider>
             <div className="App">
