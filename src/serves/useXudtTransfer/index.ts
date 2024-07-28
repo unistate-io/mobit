62f9b7a3e16dcf339279e6c9a127ab9f268b6aed
qieman <253058016@qq.com>
2024-07-28 10:35:32 +0800

    update xudtTransfer

--- src/serves/useXudtTransfer/index.ts
+++ src/serves/useXudtTransfer/index.ts
@@ -1,4 +1,3 @@
-import {Indexer} from '@ckb-lumos/lumos'
 import {useContext} from "react"
 import {CKBContext} from "@/providers/CKBProvider/CKBProvider"
 import {ccc} from "@ckb-ccc/connector-react"
@@ -6,7 +5,7 @@ import {TokenInfoWithAddress} from "@/utils/graphql/types"
 import {CkbHelper, createTransferXudtTransaction} from 'mobit-sdk'
 
 export default function useXudtTransfer() {
-    const {signer, config, network} = useContext(CKBContext)
+    const {signer, network} = useContext(CKBContext)
 
     const build = async ({
                              froms,
@@ -16,7 +15,7 @@ export default function useXudtTransfer() {
                              tokenInfo
                          }: { froms: string[], to: string, amount: string, tokenInfo: TokenInfoWithAddress, feeRate: number }) => {
 
-        const indexer = new Indexer(config.ckb_indexer, config.ckb_rpc)
+        // const indexer = new Indexer(config.ckb_indexer, config.ckb_rpc)
 
         // const txInfo = await transferTokenToAddress(
         //     froms,
@@ -36,18 +35,15 @@ export default function useXudtTransfer() {
         const _tx = createTransferXudtTransaction({
             xudtArgs: tokenInfo.address.script_args,
             receivers: [{toAddress: to, transferAmount: BigInt(amount)}],
-            ckbAddress: froms as any,
-            collector:ckbHelper.collector,
+            ckbAddresses: froms,
+            collector: ckbHelper.collector,
             isMainnet: network === 'mainnet'
-        }, BigInt(feeRate))
+        }, undefined, BigInt(feeRate))
 
         const cccLib = ccc as any
         const __tx = cccLib.Transaction.fromLumosSkeleton(_tx)
 
-
         console.log(__tx)
-
-
         return __tx
     }
 
@@ -58,19 +54,18 @@ export default function useXudtTransfer() {
                                    amount,
                                    feeRate,
                                    tokenInfo
-                               }: { froms: string[], to: string, amount: string, tokenInfo: TokenInfoWithAddress, feeRate: number}) => {
+                               }: { froms: string[], to: string, amount: string, tokenInfo: TokenInfoWithAddress, feeRate: number }) => {
         if (!signer) {
             throw new Error('Please connect wallet first')
         }
 
-        const tx = await build(
-            {
-                froms,
-                to,
-                amount,
-                feeRate,
-                tokenInfo}
-        )
+        const tx = await build({
+            froms,
+            to,
+            amount,
+            feeRate,
+            tokenInfo
+        })
         return await signer.sendTransaction(tx)
     }
 
