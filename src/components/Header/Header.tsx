450fb94eff73bc6c1902671f25d46ababbec8266
ZOU <253058016@qq.com>
2024-12-31 11:44:39 +0800

    add menu drawer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,62 +1,110 @@
 import { Link, NavLink } from "react-router-dom"
 import HeaderMenu from "@/components/HeaderMenu/HeaderMenu"
 import { CKBContext } from "@/providers/CKBProvider/CKBProvider"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { LangContext } from "@/providers/LangProvider/LangProvider"
 import { useParams } from "react-router-dom"
+import {
+    Drawer,
+    DrawerContent,
+    DrawerTrigger,
+} from "@/components/Drawer"
 
 function Header() {
     const { address } = useContext(CKBContext)
     const { lang } = useContext(LangContext)
     const { address: addressParams } = useParams()
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <header className={"sticky w-full bg-white shadow h-[60px] top-0 z-20"}>
             <div className={"max-w-[--page-with] mx-auto h-[60px] flex items-center px-3 box-border justify-between"}>
                 <div className="flex flex-row items-center">
+                    <Drawer direction={"left"} open={isOpen} onOpenChange={setIsOpen}>
+                        <DrawerTrigger>
+                            <i className="md:hidden block uil-align-justify text-2xl mr-2" />
+                        </DrawerTrigger>
+                        <DrawerContent className="bg-white w-[60vw] h-[100vh]">
+                           <div className="grid grid-cols-1 gap-4 px-3">
+                               <Link to="/">
+                                   <img src="/images/logo.png" alt="" width={114} height={32} />
+                               </Link>
+
+                               {!!address && (
+                                   <NavLink
+                                       onClick={() => setIsOpen(false)}
+                                       className='font-semibold pl-3'
+                                       to={`/address/${address}`}>
+                                      <i className=""></i> {lang["Profile"]}
+                                   </NavLink>
+                               )}
+                               <NavLink
+                                   onClick={() => setIsOpen(false)}
+                                   className='font-semibold pl-3'
+                                   to={`/market`}>
+                                   {lang["Market"]}
+                               </NavLink>
+                               <NavLink
+                                   onClick={() => setIsOpen(false)}
+                                   className='font-semibold pl-3'
+                                   to={`/apps`}>
+                                   {lang["Apps"]}
+                               </NavLink>
+                               <NavLink
+                                   onClick={() => setIsOpen(false)}
+                                   className='font-semibold pl-3'
+                                   to={`/trade`}>
+                                   {lang["Swap"]}
+                               </NavLink>
+                           </div>
+                        </DrawerContent>
+                    </Drawer>
+
                     <Link to="/">
                         <img src="/images/logo.png" alt="" width={114} height={32} />
                     </Link>
 
-                    {!!address && (
+                    <div className="hidden sm:block">
+                        {!!address && (
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive && address === addressParams ? "text-[#6CD7B2]" : ""
+                                    }`
+                                }}
+                                to={`/address/${address}`}
+                            >
+                                {lang["Profile"]}
+                            </NavLink>
+                        )}
+
                         <NavLink
                             className={({ isActive }) => {
-                                return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive && address === addressParams ? "text-[#6CD7B2]" : ""
-                                    }`
+                                return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
+                                }`
                             }}
-                            to={`/address/${address}`}
+                            to={`/market`}
                         >
-                            {lang["Profile"]}
+                            {lang["Market"]}
                         </NavLink>
-                    )}
-
-                    <NavLink
-                        className={({ isActive }) => {
-                            return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
+                        <NavLink
+                            className={({ isActive }) => {
+                                return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
                                 }`
-                        }}
-                        to={`/market`}
-                    >
-                        {lang["Market"]}
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => {
-                            return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
+                            }}
+                            to={`/apps`}
+                        >
+                            {lang["Apps"]}
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => {
+                                return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
                                 }`
-                        }}
-                        to={`/apps`}
-                    >
-                        {lang["Apps"]}
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => {
-                            return `whitespace-nowrap text-xs font-semibold ml-3 md:ml-10 md:text-sm ${isActive ? "text-[#6CD7B2]" : ""
-                                }`
-                        }}
-                        to={`/trade`}
-                    >
-                        {lang["Swap"]}
-                    </NavLink>
+                            }}
+                            to={`/trade`}
+                        >
+                            {lang["Swap"]}
+                        </NavLink>
+                    </div>
                 </div>
 
                 <HeaderMenu />
