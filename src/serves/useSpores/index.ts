812bebc4a6227bce1e90e89ca29695dd46b3bf99
ZOU <253058016@qq.com>
2025-07-04 16:13:58 +0800

    Update index.ts

--- src/serves/useSpores/index.ts
+++ src/serves/useSpores/index.ts
@@ -1,33 +1,38 @@
-import {useEffect, useRef, useState, useContext} from "react"
+import {useEffect, useRef, useState, useContext, useMemo} from "react"
 import {Spores, SporesActions} from "@/utils/graphql/types"
 import {querySporesByAddress, querySporeActionsBySporeIds} from "@/utils/graphql"
 import {CKBContext} from "@/providers/CKBProvider/CKBProvider"
 
 export interface SporesWithChainInfo extends Spores {
-    chain: 'btc' | 'ckb'
+    chain: "btc" | "ckb"
 }
 
 export default function useSpores(addresses: string[]) {
     const [data, setData] = useState<SporesWithChainInfo[]>([])
-    const [status, setStatus] = useState<'loading' | 'complete' | 'error'>('loading')
+    const [displayData, setDisplayData] = useState<SporesWithChainInfo[]>([])
+    const [fetched, setFetched] = useState(false)
+    const [status, setStatus] = useState<"loading" | "complete" | "error">("loading")
     const [error, setError] = useState<undefined | any>(undefined)
     const [page, setPage] = useState(1)
-    const [loaded, setLoaded] = useState(true)
-    const {network} = useContext(CKBContext)
-    const pageSize = 200
+    const loaded = useMemo(() => {
+        return displayData.length === data.length && fetched
+    }, [displayData, data, fetched])
 
-    const historyRef = useRef('')
+    const {network} = useContext(CKBContext)
+    const pageSize = 3
+
+    const historyRef = useRef("")
 
     const handleNextPage = (page: number) => {
-        if (status !== 'loading') {
+        if (status !== "loading") {
             setPage(page)
         }
     }
 
     useEffect(() => {
         if (!addresses || !addresses.length) return
-        if (historyRef.current !== addresses.join(',')) {
-            historyRef.current = addresses.join(',')
+        if (historyRef.current !== addresses.join(",")) {
+            historyRef.current = addresses.join(",")
             setPage(1)
             return
         }
@@ -35,31 +40,47 @@ export default function useSpores(addresses: string[]) {
 
     useEffect(() => {
         if (page === 1) {
-            setStatus('loading')
+            setStatus("loading")
         }
 
-        (async () => {
+        ;(async () => {
             try {
-                const spores = await querySporesByAddress(addresses, page, pageSize, undefined, network === 'mainnet')
-                const sporeActions = await querySporeActionsBySporeIds(spores.map((s: Spores) => s.spore_id), network === 'mainnet')
-                const checkSporesburned = spores.filter((s: Spores) => {
-                   const isBurned = sporeActions.some((a: SporesActions) => a.spore_id === s.spore_id && a.action_type === 'BurnSpore')
-                   return !isBurned
-                })
-                setLoaded(spores.length < pageSize)
-                const list = page === 1 ? checkSporesburned : [...data, ...checkSporesburned]
-                setData(list.map((s: Spores) => {
-                    return {
-                        ...s,
-                        chain: 'ckb'
-                    }
-                }))
-                setStatus('complete')
+                let _data = [...data]
+                if (!fetched) {
+                    const spores = await querySporesByAddress(
+                        addresses,
+                        1,
+                        200,
+                        undefined,
+                        network === "mainnet"
+                    )
+                    const sporeActions = await querySporeActionsBySporeIds(
+                        spores.map((s: Spores) => s.spore_id),
+                        network === "mainnet"
+                    )
+                    const checkSporesburned = spores.filter((s: Spores) => {
+                        const isBurned = sporeActions.some(
+                            (a: SporesActions) => a.spore_id === s.spore_id && a.action_type === "BurnSpore"
+                        )
+                        return !isBurned
+                    })
+                    const res = checkSporesburned.map((s: Spores) => {
+                        return {
+                            ...s,
+                            chain: "ckb"
+                        }
+                    }) as SporesWithChainInfo[]
+                    setData(res)
+                    _data = res
+                    setFetched(true)
+                }
+
+                setDisplayData(_data.slice(0, page * pageSize))
+                setStatus("complete")
             } catch (e: any) {
                 console.error(e)
                 setData([])
-                setStatus('error')
-                setLoaded(true)
+                setStatus("error")
                 setError(e)
             }
         })()
@@ -68,7 +89,7 @@ export default function useSpores(addresses: string[]) {
     return {
         setPage: handleNextPage,
         page,
-        data,
+        data: displayData,
         status,
         error,
         loaded
